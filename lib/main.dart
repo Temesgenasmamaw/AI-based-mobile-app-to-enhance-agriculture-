fb388189933f84496a99d32cdea1847dbce5beb9
Temesgenasmamaw <asmamawtemesgen16@gmail.com>
2023-05-16 19:58:56 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mango_app/pages/splash.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:mango_final/pages/language.dart';
-// import 'package:mango_app/pages/splash.dart';
 import 'package:provider/provider.dart';
 
 import 'const/theme_data.dart';
@@ -13,6 +11,8 @@ import 'pages/prediction.dart';
 import 'pages/home.dart';
 import 'theme/theme-model.dart';
 
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
